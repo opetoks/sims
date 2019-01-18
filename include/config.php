bd9db12920d5d8cfb755ec50c35e631b1ec93e5e
Opeyemi Tosin Akinluyi <opetoks@gmail.com>
2019-01-18 01:37:31 +0100

    the configuration file

--- include/config.php
+++ include/config.php
@@ -1,1 +1,13 @@
-
+<?php 
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('display_errors', 1);
+ini_set('error_log', 'errors.log');
+ini_set("error_reporting", E_ALL);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'localhost');
+define('DB_NAME', 'opetoks_esis');
+include_once ("DB.php");
+$db = DB::getInstance(); 
+?>
