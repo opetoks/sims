a47965120eb64bf76b8e3ffde1d9bf8744253404
Opeyemi Tosin Akinluyi <opetoks@gmail.com>
2019-01-18 01:39:14 +0100

    database configuration file

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
-ini_set('display_errors', 1);
+ini_set('display_errors', 1);//@@@
 ini_set('error_log', 'errors.log');
 ini_set("error_reporting", E_ALL);
 define('DB_HOST', 'localhost');
